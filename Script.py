30c95d4a8f0019e768cdbc1048154a3836c8df17
thor <87942346+Vaisakh9526574096@users.noreply.github.com>
2022-02-08 12:52:45 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -10,12 +10,13 @@ class script(object):
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
-    SOURCE_TXT = """<b>NOTE:</b>
-- Starkbotz is a not open source project. 
-- Source - @Starkbotz  
-
+      SOURCE_TXT = """<b>NOTE:</b>
+- Eva Maria is a open source project. 
+- Source - https://github.com  
+<b>DEVS:</b>
+- <a href=https://t.me/@Starkbotz>Starkbotz </a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
-
+    
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
