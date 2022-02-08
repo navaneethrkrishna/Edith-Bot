dea3b07d997adc2a4719eeed1919a78a0508c143
thor <87942346+Vaisakh9526574096@users.noreply.github.com>
2022-02-08 13:37:44 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -32,7 +32,7 @@ so dont wast time
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 - Filter is the feature were users can set automated replies for a particular keyword and tessa will respond whenever a keyword is found the message
 <b>NOTE:</b>
-1. eva maria should have admin privillage.
+1. 🄴🄳🄸🅃🄷 should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 <b>Commands and Usage:</b>
@@ -47,7 +47,7 @@ so dont wast time
 2. Eva Maria supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https//t.me/EvaMariaBot)</code>
+<code>[Button Text](buttonurl:https//t.me/🄴🄳🄸🅃🄷)</code>
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
     AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
